498132c3b7cf7fce1b4989f2c0aa88de8acbb0d0
Lorenzo Fontoura <lorenzo@nioel.com>
2020-01-10 20:59:16 +1000

    Fragment Processor for flexibility

--- Runtime/Processor.cs
+++ Runtime/Processor.cs
@@ -12,39 +12,20 @@ namespace PPS {
         void LateUpdate();
     }
 
+    /// <summary>
+    /// An independent Processor which does not handle any data owned by itself.
+    /// </summary>
     public abstract class Processor : IProcessor {
-        public abstract void Start();
-        public abstract void Update();
-        public abstract void FixedUpdate();
-        public abstract void LateUpdate();
-    }
 
-    public abstract class Processor<TSystem, TProfile> : Processor, IDisposable
-    where TSystem : ISystem
-    where TProfile : Profile {
-
-        private bool isDisposed;
         private bool isProcessing;
-        protected TSystem system;
-        protected TProfile profile;
-        protected List<Processor> subProcessors = new List<Processor>();
+        private List<Processor> subProcessors = new List<Processor>();
 
         public event EventHandler OnProcessingStart;
         public event EventHandler OnProcessingEnd;
 
-        public TProfile Profile => this.profile;
-        public TSystem System => this.system;
         protected virtual bool ProcessOnFixedUpdate => false;
         protected virtual bool ShouldProcess => true;
-
-        protected Processor(TSystem system,  TProfile profile) {
-            this.system = system;
-            this.profile = profile;
-        }
-
-        ~Processor() {
-            Dispose(false);
-        }
+        public List<Processor> SubProcessors => this.subProcessors;
 
         protected virtual void Process() { }
 
@@ -52,9 +33,9 @@ namespace PPS {
         /// Processor.Start is called when its system has been fully initialised
         /// in respect to subsystems and dependencies and is ready to operate.
         /// </summary>
-        public override void Start() { }
+        public virtual void Start() { }
 
-        public override void Update() {
+        public virtual void Update() {
             // Reverse loop due to possible Processor disposal.
             for (int i = this.subProcessors.Count; i-- > 0;) {
                 this.subProcessors[i].Update();
@@ -64,7 +45,7 @@ namespace PPS {
                 TryProcess();
         }
 
-        public override void FixedUpdate() {
+        public virtual void FixedUpdate() {
             // Reverse loop due to possible Processor disposal.
             for (int i = this.subProcessors.Count; i-- > 0;) {
                 this.subProcessors[i].FixedUpdate();
@@ -74,7 +55,7 @@ namespace PPS {
                 TryProcess();
         }
 
-        public override void LateUpdate() {
+        public virtual void LateUpdate() {
             // Reverse loop due to possible Processor disposal.
             for (int i = this.subProcessors.Count; i-- > 0;) {
                 this.subProcessors[i].LateUpdate();
@@ -98,25 +79,59 @@ namespace PPS {
 
             Process();
         }
+    }
+
+    /// <summary>
+    /// A Processor which is linked to its Profile.
+    /// </summary>
+    public abstract class Processor<TProfile> : Processor
+        where TProfile : Profile {
+
+        private bool isDisposed;
+        private readonly TProfile profile;
+
+        public TProfile Profile => this.profile;
+
+        protected Processor(TProfile profile) {
+            this.profile = profile;
+        }
+
+        ~Processor() {
+            Dispose(false);
+        }
 
         public virtual void Dispose() {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        private void Dispose(bool isDisposing) {
+        protected virtual void Dispose(bool isDisposing) {
             if (this.isDisposed || !isDisposing)
                 return;
-
-            this.system.RemoveInstance(this);
 
             if (this.profile.GameObject != null)
                 UnityEngine.Object.DestroyImmediate(this.profile.GameObject);
 
-            foreach (IDisposable subProcessor in this.subProcessors)
+            foreach (IDisposable subProcessor in SubProcessors)
                 subProcessor.Dispose();
 
             this.isDisposed = true;
+        }
+    }
+
+    /// <summary>
+    /// A Processor which is linked to its Profile and instantiating System.
+    /// </summary>
+    public abstract class Processor<TSystem, TProfile> : Processor<TProfile>
+    where TSystem : ISystem
+    where TProfile : Profile {
+
+        private readonly TSystem system;
+
+        public TSystem System => this.system;
+
+        protected Processor(TSystem system, TProfile profile) : base(profile) {
+            this.system = system;
         }
     }
 }
